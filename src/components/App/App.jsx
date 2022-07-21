0af6febf9eae25911f3ece4e46b526f7170fa2b5
Uliana Komarynska <uliakom@gmail.com>
2022-07-21 16:40:41 +0300

    fixing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,9 +19,9 @@ class App extends Component {
 
   componentDidMount() {
     const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
 
-    if (parsedContacts) {
+    if (contacts) {
+      const parsedContacts = JSON.parse(contacts);
       this.setState({ contacts: parsedContacts });
     }
   }
